7509e37d26e09dcc8b386a3dfefc702fee8cbf53
senjansen <senjansen.sj@gmail.com>
2019-07-03 20:39:06 +0700

    upload exercise 11

---
+++ exercise-11.js
@@ -0,0 +1,16 @@
+function balikKata(kata) {
+    let reversed = '';
+    
+    for (var counter = kata.length - 1; counter >= 0; counter--) {
+        reversed += kata[counter];
+    }
+    return reversed;
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
+
